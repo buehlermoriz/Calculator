f73b4c96e70a7a2b8982bce76dd587431a13d487
Moriz Buehler <buehlermoriz@gmail.com>
2021-04-02 16:12:07 +0200

    Hinzugefügte dom-utils

---
+++ src/dom-utils.ts
@@ -0,0 +1,27 @@
+//DOM utils numPad
+export const button1 = document.getElementById("button-1")! as HTMLInputElement
+export const button2 = document.getElementById("button-2")! as HTMLInputElement
+export const button3 = document.getElementById("button-3")! as HTMLInputElement
+export const button4 = document.getElementById("button-4")! as HTMLInputElement
+export const button5 = document.getElementById("button-5")! as HTMLInputElement
+export const button6 = document.getElementById("button-6")! as HTMLInputElement
+export const button7 = document.getElementById("button-7")! as HTMLInputElement
+export const button8 = document.getElementById("button-8")! as HTMLInputElement
+export const button9= document.getElementById("button-9")! as HTMLInputElement
+export const button0= document.getElementById("button-0")! as HTMLInputElement
+export const buttonComma= document.getElementById("button,")! as HTMLInputElement
+
+//DOM utils calc
+export const buttonAdd= document.getElementById("button+")! as HTMLInputElement
+export const buttonSub= document.getElementById("button-")! as HTMLInputElement
+export const buttonMult= document.getElementById("buttonX")! as HTMLInputElement
+export const buttonDev= document.getElementById("button/")! as HTMLInputElement
+
+export const buttonC= document.getElementById("buttonC")! as HTMLInputElement
+export const buttonCE= document.getElementById("buttonCE")! as HTMLInputElement
+
+export const buttonEquals= document.getElementById("button=")! as HTMLInputElement
+
+//DOM utils Tasks
+export const task= document.getElementById("task")! as HTMLInputElement
+export const scoreboard= document.getElementById("scoreboard")! as HTMLInputElement
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-const button = document.querySelector("button")! as HTMLInputElement;
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+import { button1 } from "./dom-utils";
+
 
 function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
+button1.addEventListener("click", function () {
+console.log("1")
 });
